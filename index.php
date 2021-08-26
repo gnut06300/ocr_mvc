f6faa90a144fcda62709e1820e727c2988b4b6df
Gerald Col <gnut@gnut.eu>
2021-08-26 21:17:54 +0200

    mvc complet

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+require('controller/frontend.php');
+
+try{
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPost'){
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                }
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'comment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                comment();
+            }
+            else {
+                throw new Exception('Aucun identifiant de commentaire envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'updateComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    updateComment($_GET['id'], $_POST['author'], $_POST['comment'], $_POST['post_id']);
+                }
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                throw new Exception('Aucun identifiant de commentaire envoyé');
+            }
+        }
+        
+    }
+    else{
+        listPosts();
+    }
+
+}
+catch(Exception $e){
+    echo 'Erreur :'.$e->getMessage();
+}
